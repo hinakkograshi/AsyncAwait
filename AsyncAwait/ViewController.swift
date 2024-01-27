21b92dcec8a5c5e03e765b8fe1fab6921f85008e
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-01-27 11:04:52 +0900

    add: first commit

---
+++ AsyncAwait/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AsyncAwait
+//
+//  Created by Hina on 2024/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet private weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        PrefectureData().prefectureList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = PrefectureData().prefectureList[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedPrefecture = PrefectureData().prefectureList[indexPath.row]
+
+    }
+
+}
