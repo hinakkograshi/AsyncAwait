8ab1925594892ba087bd073edcff139fcb5165d5
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-01-28 16:42:06 +0900

    add:　エラーハンドリングの修正

--- AsyncAwait/APIClient.swift
+++ AsyncAwait/APIClient.swift
@@ -14,23 +14,15 @@ struct APIClient {
         let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(Constants.apiKey)"
         let url = URL(string: urlString)
         let urlRequest = URLRequest(url: url!)
-        do {
-            let (data, response) = try await URLSession.shared.data(for: urlRequest)
-            guard let httpStatus = response as? HTTPURLResponse else {
-                throw APIClientError.networkError
-            }
 
-            // ②ステータスコードによって処理を分ける
-            switch httpStatus.statusCode {
-            case 200:
-                let decodeData = try JSONDecoder().decode(WeatherData.self, from: data)
-                description = decodeData.weather[0].main
-                cityName = decodeData.name
-                return (description, cityName)
-            default:
-                throw APIClientError.networkError
-            }
-        } catch {
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        switch (response as? HTTPURLResponse)?.statusCode {
+        case 200:
+            let decodeData = try JSONDecoder().decode(WeatherData.self, from: data)
+            description = decodeData.weather[0].main
+            cityName = decodeData.name
+            return (description, cityName)
+        default:
             throw APIClientError.networkError
         }
     }
--- AsyncAwait/WeatherViewController.swift
+++ AsyncAwait/WeatherViewController.swift
@@ -9,27 +9,25 @@ import UIKit
 
 class WeatherViewController: UIViewController {
     var prefecrure = ""
-
+    
     @IBOutlet private weak var weatherLabel: UILabel!
-
+    
     @IBOutlet private weak var prefectureLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     @IBAction private func tappedFetchWeatherButton(_ sender: UIButton) {
         showWeatherView()
     }
     func showWeatherView() {
+        let (latitude, longitude) = PrefectureLatLon().fetchLatLon(weatherPrefecture: prefecrure)
         Task { @MainActor in
-            let (latitude, longitude) = PrefectureLatLon().fetchLatLon(weatherPrefecture: prefecrure)
             do {
                 let (description, cityName) = try await APIClient().getWeatherFromAPI(latitude: latitude, longitude: longitude)
-                DispatchQueue.main.async {
-                    self.weatherLabel.text = description
-                    self.prefectureLabel.text = cityName
-                }
+                self.weatherLabel.text = description
+                self.prefectureLabel.text = cityName
             } catch {
                 let alert = AlertMaker().make(didTapRetry: {
                     self.showWeatherView()
