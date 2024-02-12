0f8a4fb9e7f6220d0a7e8bfe9631e5f1502e6bab
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-02-12 21:58:31 +0900

    add: 警告を修正

--- AsyncAwait/ErrorChangeViewController.swift
+++ AsyncAwait/ErrorChangeViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class ErrorChangeViewController: UIViewController {
     var prefecture = ""
-    @IBOutlet weak var weatherLabel: UILabel!
-    @IBOutlet weak var prefectureLabel: UILabel!
+    @IBOutlet private weak var weatherLabel: UILabel!
+    @IBOutlet private weak var prefectureLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
     }
 
-    @IBAction func didTapFetchWeather(_ sender: UIButton) {
+    @IBAction private func didTapFetchWeather(_ sender: UIButton) {
         let (latitude, longitude) = PrefectureLatLon().fetchLatLon(weatherPrefecture: prefecture)
         Task {
             do {
                 let (description, cityName) = try await WeatherAPIClient().getWeather(latitude: latitude, longitude: longitude)
                 self.weatherLabel.text = description
                 self.prefectureLabel.text = cityName
+                view.backgroundColor = .systemBlue
 
             } catch {
                 view.backgroundColor = .red
